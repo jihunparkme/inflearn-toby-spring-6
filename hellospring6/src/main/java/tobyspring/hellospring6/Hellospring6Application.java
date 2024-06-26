b7650ae90b6baa0420ac2f014a0d5a15c5820c20
Jihun Park <jihunpark.tech@gmail.com>
2024-06-26 22:17:18 +0900

    [toby spring 6] init project

---
+++ hellospring6/src/main/java/tobyspring/hellospring6/Hellospring6Application.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellospring6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hellospring6Application.class, args);
+    }
+
+}
