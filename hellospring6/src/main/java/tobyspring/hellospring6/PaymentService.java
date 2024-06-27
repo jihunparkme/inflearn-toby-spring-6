d2e82ec6a29da6a895ebad8bf704de30c35082b5
Jihun Park <jihunpark.tech@gmail.com>
2024-06-27 11:10:21 +0900

    [toby spring 6] feat: PaymentService 개발 (3)
    해외직구를 위한 원화 결제 준비 기능

--- hellospring6/src/main/java/tobyspring/hellospring6/PaymentService.java
+++ hellospring6/src/main/java/tobyspring/hellospring6/PaymentService.java
@@ -1,34 +1,51 @@
 package tobyspring.hellospring6;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
 
     /**
      * 해외직구를 위한 원화 결제 준비 기능
      *
-     * 주문번호, 외국 통화 종류, 외국 통화 기준 결제 금액을 전달 받아서 다음의 정보를 더해 Payment를 생성한다
-     * - 적용 환율
-     * - 원화 환산 금액
-     * - 원화 환산 금액 유효시간
-     *
-     * @param orderId
-     * @param currency
-     * @param foreignCurrencyAmount
-     * @return
+     * @param orderId 주문번호
+     * @param currency 외국 통화 종류
+     * @param foreignCurrencyAmount 외국 통화 기준 결제 금액
+     * @return Payment
      */
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
-        // TODO: 환율 가져오기
-        // TODO: 금액 계산
-        // TODO: 유효 시간 계산
-        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
+    public Payment prepare(final Long orderId, final String currency, final BigDecimal foreignCurrencyAmount) throws IOException {
+        // 환율 가져오기
+        final URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        final BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        final String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        final ObjectMapper mapper = new ObjectMapper();
+        final ExRateData data = mapper.readValue(response, ExRateData.class);
+        final BigDecimal exRate = data.rates().get("KRW");
+
+        // 원화 환산 금액 계산
+        final BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
+        // 원화 환산 금액 유효 시간 계산
+        final LocalDateTime validUtil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUtil);
     }
 
     /**
      * 2. 메소드 검증
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final PaymentService paymentService = new PaymentService();
         final Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
