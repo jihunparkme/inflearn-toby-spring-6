f32e4577d35f9cde56af818dc66f0c302c47a0f5
Jihun Park <jihunpark.tech@gmail.com>
2024-06-28 15:14:13 +0900

    [toby spring 6] feat: 스프링 컨테이너와 의존관계 주입

--- hellospring6/src/main/java/tobyspring/hellospring6/Client.java
+++ hellospring6/src/main/java/tobyspring/hellospring6/Client.java
@@ -1,12 +1,15 @@
 package tobyspring.hellospring6;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        final ObjectFactory objectFactory = new ObjectFactory();
-        final PaymentService paymentService = objectFactory.paymentService();
+        final BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        final PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         final Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
