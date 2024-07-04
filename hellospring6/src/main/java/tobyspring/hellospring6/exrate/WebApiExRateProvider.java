d2fabeb718af150eee587552bbab7cf68daf016b
Jihun Park <jihunpark.tech@gmail.com>
2024-07-04 11:11:34 +0900

    [toby spring 6] refactor: ApiTemplate 분리

--- hellospring6/src/main/java/tobyspring/hellospring6/exrate/WebApiExRateProvider.java
+++ hellospring6/src/main/java/tobyspring/hellospring6/exrate/WebApiExRateProvider.java
@@ -1,44 +1,19 @@
 package tobyspring.hellospring6.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import tobyspring.hellospring6.api.ApiExecutor;
+import tobyspring.hellospring6.api.ApiTemplate;
 import tobyspring.hellospring6.api.ErApiExRateExtractor;
-import tobyspring.hellospring6.api.ExRateExtractor;
-import tobyspring.hellospring6.api.SimpleApiExecutor;
+import tobyspring.hellospring6.api.HttpClientApiExecutor;
 import tobyspring.hellospring6.payment.ExRateProvider;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class WebApiExRateProvider implements ExRateProvider {
+    private ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(final String currency) {
         final String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
-    }
-
-    private static BigDecimal runApiForExRate(final String url, final ApiExecutor apiExecutor, final ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
 }
