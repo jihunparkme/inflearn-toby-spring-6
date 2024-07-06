520cc76f7d38b40ffa1b6c1ff1f50767d567b0ea
Jihun Park <jihunpark.tech@gmail.com>
2024-07-06 16:42:50 +0900

    [toby spring 6] feat: Order 리포지토리와 예외

--- hellospring6/src/main/java/tobyspring/hellospring6/DataClient.java
+++ hellospring6/src/main/java/tobyspring/hellospring6/DataClient.java
@@ -1,9 +1,8 @@
 package tobyspring.hellospring6;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring6.data.OrderRepository;
 import tobyspring.hellospring6.order.Order;
 
 import java.math.BigDecimal;
@@ -11,17 +10,14 @@ import java.math.BigDecimal;
 public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+        OrderRepository repository = beanFactory.getBean(OrderRepository.class);
 
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        Order order = new Order("100", BigDecimal.TEN);
+        repository.save(order);
 
-        final Order order = new Order("100", BigDecimal.TEN);
-        em.persist(order); // select next value for orders_SEQ
+        System.out.println(order);
 
-        System.out.println(order); // Order{id=1, no='100', total=10}
-
-        em.getTransaction().commit(); // insert into orders (no,total,id) values (?,?,?)
-        em.clear();
+        Order order2 = new Order("100", BigDecimal.ONE);
+        repository.save(order2);
     }
 }
---
+++ hellospring6/src/main/java/tobyspring/hellospring6/data/OrderRepository.java
@@ -0,0 +1,30 @@
+package tobyspring.hellospring6.data;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import tobyspring.hellospring6.order.Order;
+
+public class OrderRepository {
+    private final EntityManagerFactory emf;
+
+    public OrderRepository(final EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    public void save(Order order) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        try {
+            em.persist(order);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction.isActive()) transaction.rollback();
+            throw e;
+        } finally {
+            if (em.isOpen()) em.clear();
+        }
+    }
+}
