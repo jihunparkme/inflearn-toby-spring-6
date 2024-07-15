c717d3fd957ba697be918cc738499b73874bd22e
Jihun Park <jihunpark.tech@gmail.com>
2024-07-15 20:09:46 +0900

    [toby spring 6] feat: @Transactional과 AOP

--- hellospring6/src/main/java/tobyspring/hellospring6/OrderConfig.java
+++ hellospring6/src/main/java/tobyspring/hellospring6/OrderConfig.java
@@ -3,17 +3,17 @@ package tobyspring.hellospring6;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.hellospring6.data.JdbcOrderRepository;
 import tobyspring.hellospring6.order.OrderRepository;
 import tobyspring.hellospring6.order.OrderService;
 import tobyspring.hellospring6.order.OrderServiceImpl;
-import tobyspring.hellospring6.order.OrderServiceTxProxy;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
     @Bean
     public OrderRepository orderRepository(DataSource dataSource) {
@@ -21,13 +21,7 @@ public class OrderConfig {
     }
 
     @Bean
-    public OrderService orderService(
-            final PlatformTransactionManager transactionManager,
-            final OrderRepository orderRepository
-    ) {
-        return new OrderServiceTxProxy(
-                new OrderServiceImpl(orderRepository),
-                transactionManager
-        );
+    public OrderService orderService(final OrderRepository orderRepository) {
+        return new OrderServiceImpl(orderRepository);
     }
 }
