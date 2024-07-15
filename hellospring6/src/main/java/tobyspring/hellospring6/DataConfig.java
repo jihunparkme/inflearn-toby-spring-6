02527c0f1c6bf4d3ac38414a738ad482cbdb3846
Jihun Park <jihunpark.tech@gmail.com>
2024-07-15 09:55:02 +0900

    [toby spring 6] feat: JDBC 데이터 액세스 기술
    JPA -> JDBC

--- hellospring6/src/main/java/tobyspring/hellospring6/DataConfig.java
+++ hellospring6/src/main/java/tobyspring/hellospring6/DataConfig.java
@@ -1,16 +1,10 @@
 package tobyspring.hellospring6;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -23,34 +17,7 @@ public class DataConfig {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-
-        emf.setDataSource(dataSource());
-        emf.setPackagesToScan("tobyspring.hellospring6");
-        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter() {{
-            setDatabase(Database.H2);
-            setGenerateDdl(true);
-            setShowSql(true);
-        }});
-
-        return emf;
-    }
-
-    /**
-     * Persistence Annotation 어노테이션을 처리해주는 후처리 작업
-     */
-    @Bean
-    public BeanPostProcessor persistenceAnnotationBeanPostProcessor() {
-        return new PersistenceAnnotationBeanPostProcessor();
-    }
-
-    /**
-     * JPA와 관련된 트랜잭션을 관리해주는 빈
-     * - EntityManager transaction begin, commit 작업을 적절한 타이밍에 자동으로 수행
-     */
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        return new JpaTransactionManager(emf);
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
 }
--- hellospring6/src/main/java/tobyspring/hellospring6/OrderConfig.java
+++ hellospring6/src/main/java/tobyspring/hellospring6/OrderConfig.java
@@ -4,20 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
-import tobyspring.hellospring6.data.JpaOrderRepository;
+import tobyspring.hellospring6.data.JdbcOrderRepository;
 import tobyspring.hellospring6.order.OrderRepository;
 import tobyspring.hellospring6.order.OrderService;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
 public class OrderConfig {
     @Bean
-    public OrderRepository orderRepository() {
-        return new JpaOrderRepository();
+    public OrderRepository orderRepository(DataSource dataSource) {
+        return new JdbcOrderRepository(dataSource);
     }
 
     @Bean
-    public OrderService orderService(final PlatformTransactionManager transactionManager) {
-        return new OrderService(orderRepository(), transactionManager);
+    public OrderService orderService(
+            final PlatformTransactionManager transactionManager,
+            final OrderRepository orderRepository
+    ) {
+        return new OrderService(orderRepository, transactionManager);
     }
 }
